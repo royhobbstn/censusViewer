fc222627b91871ac582c9e033be1d6d86915a50f
Daniel Trone <danieljtrone@gmail.com>
2018-01-16 13:12:04 +0000

    config changes

--- src/_Config_JSON/configuration.mjs
+++ src/_Config_JSON/configuration.mjs
@@ -1,56 +1,55 @@
 export const configuration = {
     datasets: {
-        /*acs1014: {
+        acs1014: {
             id: 'acs1014',
-            cdn: 'd2y228x0z69ksn.cloudfront.net',
+            cdn: 's3-us-west-2.amazonaws.com/s3db-v2-acs1014',
             year: '2014',
             label: 'ACS 2010 - 2014'
-        },*/
+        },
         acs1115: {
             id: 'acs1115',
-            cdn: 'd1r5yvgf798u6b.cloudfront.net',
+            cdn: 's3-us-west-2.amazonaws.com/s3db-v2-acs1115',
             year: '2015',
             label: 'ACS 2011 - 2015'
-        }/*,
+        },
         acs1216: {
             id: 'acs1216',
-            cdn: 'd23tgl2ix1iyqu.cloudfront.net',
+            cdn: 's3-us-west-2.amazonaws.com/s3db-v2-acs1216',
             year: '2016',
             label: 'ACS 2012 - 2016'
-        }*/
+        }
     },
     geography: {
-    /*
         state: {
             id: 'state',
             sumlev: '040',
             label: 'State'
-        },*/
+        },
         county: {
             id: 'county',
             sumlev: '050',
             label: 'County'
         },
-        /*bg: {
+        bg: {
             id: 'bg',
             sumlev: '150',
             label: 'Block Group'
-        },*/
+        },
         tract: {
             id: 'tract',
             sumlev: '140',
             label: 'Tract'
-        }/*,
+        },
         place: {
             id: 'place',
             sumlev: '160',
             label: 'Place'
-        }*/
+        }
     },
-    tiles: 's3-us-west-2.amazonaws.com/vtiles-static',
+    tiles: 's3-us-west-2.amazonaws.com/geo-tilesets',
     startup: {
         source_geography: 'county',
-        source_dataset: 'acs1115',
+        source_dataset: 'acs1216',
         selected_attr: 'mhi',
     }
 
